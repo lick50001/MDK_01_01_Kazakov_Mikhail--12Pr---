169dd738336406988404f79322898909a0452778
qwel0rd <secondofall22@rambler.ru>
2025-10-20 18:06:58 +0500

    Программирование окна MainWindow. Казаков

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Book_Kazakov.Classes;
 
 namespace Book_Kazakov
 {
@@ -20,9 +22,77 @@ namespace Book_Kazakov
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Classes.Author> AllAuthors = Classes.Author.AllAuthors();
+        List<Classes.Genre> AllGenres = Classes.Genre.AllGenres();
+        List<Classes.Book> AllBooks = Classes.Book.AllBook();
         public MainWindow()
         {
             InitializeComponent();
+            AddAuthors();
+            AddGenres();
+            AddYear();
+            CreateUI(AllBooks);
+        }
+        public void AddAuthors()
+        {
+            cbAuthors.Items.Add("Выберите ...");
+            foreach (Classes.Author Author in AllAuthors)
+                cbAuthors.Items.Add(Author.FIO);
+        }
+        
+        public void AddGenres()
+        {
+            cbGenres.Items.Add("Выберите ...");
+            foreach (Classes.Genre Genre in AllGenres)
+                cbGenres.Items.Add(Genre.Name);
+        }
+
+        public void AddYear()
+        {
+            cbYear.Items.Add("Выберите ...");
+            List<int> AllYears = new List<int>();
+            foreach (Classes.Book Book in AllBooks)
+                if (AllYears.Find(x => x == Book.Year) == 0)
+                {
+                    AllYears.Add(Book.Year);
+                    cbYear.Items.Add(Book.Year);
+                }
+        }
+
+        public void CreateUI(List<Classes.Book> AllBooks)
+        {
+            parent.Children.Clear();
+            foreach (Classes.Book Book in AllBooks)
+                parent.Children.Add(new Elements.Element(Book));
+        }
+
+        public void Search_Book(object seder, KeyEventArgs e) =>
+            Search();
+
+        private void SelectAuthor(object sender, SelectionChangedEventArgs e) =>
+            Search();
+
+        public void Search()
+        {
+            List<Classes.Book> FindBook = AllBooks.FindAll(x => x.Name.ToLower().Contains(tbSearch.Text.ToLower()));
+
+            if (cbAuthors.SelectedIndex > 0)
+            {
+                Classes.Author SelectAuthor = AllAuthors.Find(x => x.FIO == cbAuthors.SelectedItem.ToString());
+                FindBook = FindBook.FindAll(x => x.Authors.Find(y => y.Id == SelectAuthor.Id) != null);
+            }
+
+            if (cbGenres.SelectedIndex > 0)
+            {
+                Classes.Genre SelectGender = AllGenres.Find(x => x.Name == cbGenres.SelectedItem.ToString());
+                FindBook = FindBook.FindAll(x => x.Genres.Find(y => y.Id == SelectGender.Id) != null);
+            }
+
+            if (cbYear.SelectedIndex > 0)
+            {
+                FindBook = FindBook.FindAll(x => x.Year == Convert.ToInt32(cbYear.SelectedItem.ToString()));
+                CreateUI(FindBook);
+            } 
         }
     }
 }
